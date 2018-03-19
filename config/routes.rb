7310e04edd6e9e47f3bfe43ee4fb82ddafc731df
kmflucero <kevin.lucero@gmail.com>
2018-03-19 17:39:57 -0300

    dsajksa

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-
 	resources :posts do
 		resources :comments, only: [:create, :destroy, :update, :edit]
 	end
-
-	devise_for :users, controllers: {
-		sessions: 'users/sessions',
-		registrations: 'users/registrations'
-	}
-
+	devise_for :users
 	root 'posts#index'
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
